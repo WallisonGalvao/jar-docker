0291c0742cdaaa5bc74053acc7be8d784f3bc307
Quero <wquero@deloitte.com>
2022-05-24 21:21:22 -0300

    ip container

--- testeProjeto/src/main/java/com/sptech/testeprojeto/Connection.java
+++ testeProjeto/src/main/java/com/sptech/testeprojeto/Connection.java
@@ -11,7 +11,7 @@ public class Connection {
         
 
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://172.18.0:3306/smartsac?autoReconnect=true&useSSL=false");
+        dataSource.setUrl("jdbc:mysql://172.18.0.1:3306/smartsac");
         dataSource.setUsername("root");
         dataSource.setPassword("smartsac");
 
